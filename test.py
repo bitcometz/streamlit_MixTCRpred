0630eb0e2f9a992c5e22467826b1919cc75eb383
bitcometz <523194963@qq.com>
2024-05-14 13:32:35 +0800

    test version

---
+++ test.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import os
+from argparse import ArgumentParser
+from tabulate import tabulate
+import wget
+import pickle
+import sys
+import pathlib
+import configparser
+import pandas as pd
+
+st.write("hello, world!")
