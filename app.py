0db7ac7c3d9b6f01c74f682d0f534a3ab4da341f
bitcometz <novogene@163.com>
2024-05-15 15:30:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import subprocess
+import os
 
 python_path = subprocess.run(["which", "python"], stdout=subprocess.PIPE, text=True).stdout.strip()
 cmd_line     = " ".join([python_path, "aa.py"])
