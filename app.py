e5ae539636d87d61b9aabb59eccf613ccbedf853
bitcometz <novogene@163.com>
2024-05-15 15:28:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 import streamlit as st
-import pkg_resources
+import subprocess
 
-# 获取当前 Python 环境中已安装的所有包
-installed_packages = pkg_resources.working_set
-
-# 打印已安装的包名和版本信息
-for package in installed_packages:
-    st.write(package.key, package.version)
-    
-from tabulate import tabulate
+python_path = subprocess.run(["which", "python"], stdout=subprocess.PIPE, text=True).stdout.strip()
+cmd_line     = " ".join([python_path, "aa.py"])
+return_code  = os.system(cmd_line)
